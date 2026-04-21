3236d7e3193abfc360cb36ecc7cfc6e514b24581
Mohamed Tamer <mohamed.salem051@eng-st.cu.edu.eg>
2026-04-22 00:10:15 +0200

    final touch (cleaning)

--- notebooks/helpers/data_loader.py
+++ notebooks/helpers/data_loader.py
@@ -15,7 +15,10 @@ class DataLoader:
     def processed(filename: str) -> Path:
         return DataLoader.PROCESSED_DIR / filename
 
-STAGE2_PATH = Path('../../data/interim/cleaning_stage2.csv')
+STAGE1_PATH = Path('../../data/interim/cleaning/cleaning_stage1.csv')
+STAGE1_LOG_PATH = Path('../../data/interim/cleaning/cleaning_stage1_log.csv')
+STAGE2_PATH = Path('../../data/interim/cleaning/cleaning_stage2.csv')
+STAGE2_LOG_PATH = Path('../../data/interim/cleaning/cleaning_stage2_log.csv')
 PROCESSED_PATH = Path('../../data/processed/merged_inspections_licenses_inner_clean.csv')
 PROCESSED_PARQUET_PATH = Path('../../data/processed/merged_inspections_licenses_inner_clean.parquet')
 LOG_PATH = Path('../../data/interim/cleaning_log.csv')
